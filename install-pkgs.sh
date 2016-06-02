2e8ea82324541fd52e767200e3695fb2fee25af9
apifyedi <apifyedi@gmail.com>
2016-06-02 08:06:05 -0400

    Update install-pkgs.sh

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Black Smith!.."
+
+echo "Installing S3FS .."
+sudo wget http://s3fs.googlecode.com/files/s3fs-1.74.tar.gz
+cd s3fs-1.74/
+sudo ./configure --prefix=/opt
+sudo make
+sudo make install
+
+# configure AWS credentials
+
